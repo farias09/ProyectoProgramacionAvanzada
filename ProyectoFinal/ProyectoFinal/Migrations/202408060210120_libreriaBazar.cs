f08ecb92f8b76873fa77a347fc47f33622911a6c
BryanMJ04 <109931931+BryanMJ04@users.noreply.github.com>
2024-08-05 20:51:11 -0600

    Registro y Login

---
+++ ProyectoFinal/ProyectoFinal/Migrations/202408060210120_libreriaBazar.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoFinal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class libreriaBazar : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
